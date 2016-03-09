5c1dc9d56ae41ee5d66d7b27e6c6038a9f548ee6
Christopher Baker <me@christopherbaker.net>
2016-03-08 23:09:20 -0600

    Add initial docs.

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxCloudPlatform
+	ADDON_DESCRIPTION = A set of tools for building access to Google Cloud Platform services.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "http" "cloud" "google" "api"
+	ADDON_URL = http://github.com/bakercp/ofxCloudPlatform
+
+common:
+	ADDON_DEPENDENCIES = ofxHTTP ofxSSLManager ofxIO ofxMediaType ofxTaskQueue ofxNetworkUtils ofxGeo
