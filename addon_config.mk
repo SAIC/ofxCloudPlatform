ef2e878a9fba020e42868e35ecc629737759e0e9
Christopher Baker <me@christopherbaker.net>
2018-04-18 23:18:10 -0500

    Update basics, CI.

--- addon_config.mk
+++ addon_config.mk
@@ -2,7 +2,7 @@ meta:
 	ADDON_NAME = ofxCloudPlatform
 	ADDON_DESCRIPTION = A set of tools for building access to Google Cloud Platform services.
 	ADDON_AUTHOR = bakercp
-	ADDON_TAGS = "http" "cloud" "google" "api"
+	ADDON_TAGS = "http" "cloud" "google" "api" "computer vision"
 	ADDON_URL = http://github.com/bakercp/ofxCloudPlatform
 
 common:
